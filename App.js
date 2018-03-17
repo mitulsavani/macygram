34f4c3d14789279cf49eaf597fa333f5b2704606
mitulsavani <mitul.savani2@yahoo.com>
2018-03-17 15:01:58 -0700

    Added IntroScreen, now everone can pull it

--- App.js
+++ App.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import IntroScreen from './app/Screens/IntroScreen';
 
 export default class App extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      screen: 'Intro'
+    }
+  }
+
+
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Macy gram</Text>
-      </View>
-    );
+
+    const {screen} = this.state
+
+    if(screen === 'Intro'){
+      return <IntroScreen/>
+    }
+    else 
+    {
+      return (
+        <View style={styles.container}>
+          <Text>Open up App.js to start working on your app!</Text>
+        </View>
+      )
+    }
   }
 }
 
